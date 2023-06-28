73b5204a3aa0b9369799fafd21cc4d7d3dde3e1c
cicilianataliadesima <92568542+adeliarizki@users.noreply.github.com>
2023-06-28 09:38:46 +0700

    Initial commit

---
+++ app/src/main/java/com/adeliarizki202102253/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.adeliarizki202102253.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
